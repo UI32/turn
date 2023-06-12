5b66100a31cece92fc251f41cb508d6f1a6e8c2f
Juan_Lou <mettheland@gmail.com>
2023-06-12 14:24:44 +0200

    sketchfab viewer embedded

--- src/ui/components/Model3D.jsx
+++ src/ui/components/Model3D.jsx
@@ -5,7 +5,12 @@ const Model3D = ({}) => {
   const t = useTranslations();
   return (
     <div className="model" id="model" data-aos="custom-animation">
-      <div className="model-wrapper"></div>
+      <div className="model-wrapper">
+        <iframe
+          style={{ width: "100%", height: "100%", border: "none" }}
+          src="https://sketchfab.com/models/5ca574f7b6f24d7ab88a8ba6ab6eb0bb/embed?scrollwheel=0&autospin=1&autostart=1&camera=0&ui_hint=0&ui_stop=0"
+        ></iframe>
+      </div>
       <div className="panel info-panel">
         <p className="lead-m">{t("model:title")}</p>
         <p
