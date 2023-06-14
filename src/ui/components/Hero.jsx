4b34a6cdeaf76b329affe66f59e46bc2f769b7a8
Juan_Lou <mettheland@gmail.com>
2023-06-14 19:08:20 +0200

    Refactor and clean up

--- src/ui/components/Hero.jsx
+++ src/ui/components/Hero.jsx
@@ -2,39 +2,25 @@ import React from "react";
 import useTranslations from "../../hooks/useTranslations";
 import { Replay } from "vimond-replay";
 import CompoundVideoStreamer from "vimond-replay/video-streamer/compound";
+import {
+  videoSource,
+  videoPlayerOptions,
+  initialPlaybackProps,
+} from "../../config/videoPlayerConfig";
 
 const Hero = () => {
   let playVideo, setPlaybackPosition;
 
-  const playbackActionsReady = ({ play, setPosition }) => {
+  const handlePlaybackActionsReady = ({ play, setPosition }) => {
     playVideo = play;
     setPlaybackPosition = setPosition;
   };
-  const restartVideo = () => {
-    if (setPlaybackPosition && playVideo) {
+
+  const handleStreamStateChange = state => {
+    if (state.position > 17.05 && setPlaybackPosition && playVideo) {
       setPlaybackPosition(0);
       playVideo();
     }
-  };
-
-  const videoPlayerOptions = {
-    controls: { includeControls: [] },
-    videoStreamer: {
-      // Adaptative playback config.
-      shakaPlayer: {
-        customConfiguration: {
-          streaming: {
-            bufferingGoal: 120,
-          },
-        },
-      },
-      hlsjs: {
-        customConfiguration: {
-          capLevelToPlayerSize: true,
-          maxBufferLength: 45,
-        },
-      },
-    },
   };
 
   const t = useTranslations();
@@ -55,31 +41,15 @@ const Hero = () => {
       <div className="hero-bg" data-aos="custom-animation">
         <div className="videoloop">
           <Replay
-            onPlaybackActionsReady={playbackActionsReady}
-            onStreamStateChange={state => {
-              if (state.position > 17.05) restartVideo();
-            }}
-            source={{
-              streamUrl:
-                "https://turn2x.s3.eu-north-1.amazonaws.com/turnDroneVideo.mp4",
-              contentType: "video/mp4",
-            }}
+            onPlaybackActionsReady={handlePlaybackActionsReady}
+            onStreamStateChange={handleStreamStateChange}
+            source={videoSource}
+            initialPlaybackProps={initialPlaybackProps}
             options={videoPlayerOptions}
-            initialPlaybackProps={{
-              isMuted: true,
-              isPaused: false,
-            }}
           >
             <CompoundVideoStreamer />
           </Replay>
         </div>
-        {/* <video className="videoloop" autoPlay loop muted playsInline>
-          <source
-            src={
-              "https://turn2x.s3.eu-north-1.amazonaws.com/turnDroneVideo.mp4"
-            }
-          />
-        </video> */}
       </div>
     </div>
   );
