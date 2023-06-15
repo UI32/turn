8ebc8320fd9586b753687b23bb5f24c31bca4193
Juan_Lou <mettheland@gmail.com>
2023-06-15 15:33:09 +0200

    fix branch name in script

--- scripts/deployment.sh
+++ scripts/deployment.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 :'
-This is going to build the proyect, copy the public folder to docs, 
-delete the public folder, stash current changes, checkout to deploy, 
-pull from dev, add all, commit, push and go to dev, 
+This is going to build the project, copy the public folder to docs, 
+delete the public folder, stash current changes, checkout to 
+deployment branch, pull from dev, add all, commit, push and go to dev, 
 '
 
 npm run build &&
 cp -r ./public ./docs &&
 rm -r ./public &&
 git stash &&
-git checkout deploy &&
+git checkout deployment &&
 git pull dev &&
 git add -all &&
 git commit -m 'update' &&
