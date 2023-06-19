ff2bf7e5a80854208ebcc565fd4cf50825ea8654
Juan_Lou <mettheland@gmail.com>
2023-06-19 12:46:37 +0200

    added password

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import AppWrapper from "../ui/layouts/AppWarpper";
 import HelmetComponent from "../ui/layouts/HelmetComponent";
 import metaTags from "../config/meta-tags/en.json";
 import { getLocaleFromPathname } from "../libs/i18n.js";
 import HomePage from "../ui/pages/HomePage";
+import { validInput } from "../utils/entryCheck";
+import NotFoundPage from "./404";
 
 const IndexPage = props => {
   const locale = getLocaleFromPathname(props.location);
+
+  const userInput = window.prompt("Enter password: ");
+  const result = validInput(userInput);
 
   return (
     <>
@@ -17,7 +22,7 @@ const IndexPage = props => {
         page=""
       />
       <AppWrapper {...props} lang={locale}>
-        <HomePage />
+        {result ? <HomePage /> : <NotFoundPage />}
       </AppWrapper>
     </>
   );
