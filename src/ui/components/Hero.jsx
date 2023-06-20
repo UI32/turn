313c1e21cc43f32be84a00bfac657c7c817868a4
Juan_Lou <mettheland@gmail.com>
2023-06-20 15:17:34 +0200

    Vimeo testing

--- src/ui/components/Hero.jsx
+++ src/ui/components/Hero.jsx
@@ -18,13 +18,30 @@ const Hero = () => {
         </div>
       </div>
       <div className="hero-bg" data-aos="custom-animation">
-        <video autoPlay loop muted playsInline>
+        <div style={{ padding: 0, position: "relative" }}>
+          <iframe
+            src="https://player.vimeo.com/video/837660645?h=b98fff32c2&mute=1&badge=0&autopause=0&background=1&player_id=0&app_id=58479&controls=0&autoplay=1&loop=1"
+            allow="autoplay"
+            allowFullScreen
+            style={{
+              border: "none",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100vw",
+              aspectRatio: "16/9",
+            }}
+            title="turnDroneVideo"
+          ></iframe>
+        </div>
+        <script src="https://player.vimeo.com/api/player.js"></script>
+        {/* <video autoPlay loop muted playsInline>
           <source
             src={
               "https://turn2x.s3.eu-north-1.amazonaws.com/turnDroneVideo.mp4"
             }
           />
-        </video>
+        </video> */}
       </div>
     </div>
   );
