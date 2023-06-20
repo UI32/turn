596bc3dea1d47972dd7ff6e3723ac99bce0c53f9
Juan_Lou <mettheland@gmail.com>
2023-06-20 19:53:03 +0200

    2nd video option

--- src/ui/components/Hero.jsx
+++ src/ui/components/Hero.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect } from "react";
 import useTranslations from "../../hooks/useTranslations";
 
 const Hero = () => {
@@ -22,6 +22,9 @@ const Hero = () => {
           <iframe
             src="https://player.vimeo.com/video/837660645?h=b98fff32c2&mute=1&badge=0&autopause=0&background=1&player_id=0&app_id=58479&controls=0&autoplay=1&loop=1"
             allow="autoplay"
+            onPlaying={() => {
+              console.log("jarl");
+            }}
             allowFullScreen
             style={{
               border: "none",
@@ -33,15 +36,25 @@ const Hero = () => {
             }}
             title="turnDroneVideo"
           ></iframe>
+          {/* // DANIELS VIDEO EMBEDDING
+          <iframe
+            src="https://player.vimeo.com/video/837842705?badge=0&autopause=0&player_id=0&app_id=58479"
+            onPlaying={() => {
+              console.log("jarl");
+            }}
+            allowFullScreen
+            style={{
+              border: "none",
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100vw",
+              aspectRatio: "16/9",
+            }}
+            title="TURN2X"
+          ></iframe> */}
         </div>
         <script src="https://player.vimeo.com/api/player.js"></script>
-        {/* <video autoPlay loop muted playsInline>
-          <source
-            src={
-              "https://turn2x.s3.eu-north-1.amazonaws.com/turnDroneVideo.mp4"
-            }
-          />
-        </video> */}
       </div>
     </div>
   );
