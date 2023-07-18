fb202b949e31a4696c6abc3415b19a597a8e2d1a
Sara Moreno <sara.morenoj@gmail.com>
2023-07-18 14:16:01 +0200

    add carousel module

---
+++ src/ui/components/Carousel.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+const Carousel = ({ children }) => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    fade: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 7000,
+    arrows: false,
+  };
+
+  return <Slider {...settings}>{children}</Slider>;
+};
+
+export default Carousel;
