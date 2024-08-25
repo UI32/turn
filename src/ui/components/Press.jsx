368c0449c63b4943ec692c50415e429a65740715
Alex Barakhov <alex.barakhov@tui.com>
2024-08-25 18:49:47 +0200

    added press item first element and transitions

--- src/ui/components/Press.jsx
+++ src/ui/components/Press.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import PressItem from "./PressItem";
+import useTranslations from "../../hooks/useTranslations";
 import imgEnergy from "../../assets/images/press-item-energy.jpg";
 import imgMariti from "../../assets/images/press-item-maritime.jpg";
 import imgUtilit from "../../assets/images/press-item-utilities.jpg";
 
 const Press = () => {
+  const t = useTranslations();
   return (
     <section className="press">
       <div className="press-wrapper">
         <div className="press-container">
-          <div className="press-item">intro</div>
+
+          <div className="press-item press-item-first">
+            <div className="press-item-heading">
+              <div className="press-item-pretitle">
+                <p className="lead-m">{t("press:label")}</p>
+              </div>
+              <h3 className="press-item-title alpha">{t("press:title")}</h3>
+              <p className="press-item-text text-xl">{t("press:text")}</p>
+            </div>
+          </div>
+
           <PressItem
             to="/"
             pretitle="press:item-1-pre"
