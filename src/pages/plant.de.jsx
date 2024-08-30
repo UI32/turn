80404fa7fef7c999133c3166a7324b000bf4552d
Alex Barakhov <alex.barakhov@tui.com>
2024-08-30 19:11:50 +0200

    wip: plant page added de page

---
+++ src/pages/plant.de.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import AppWrapper from "../ui/layouts/AppWarpper";
+import HelmetComponent from "../ui/layouts/HelmetComponent";
+import metaTags from "../config/meta-tags/en.json";
+import CommercialPlant from "../ui/pages/CommercialPlant";
+
+const Plant = props => {
+  return (
+    <>
+      <HelmetComponent
+        title={metaTags.plantTitle}
+        description={metaTags.plant}
+        page="/plant"
+        keywords={metaTags.keys}
+      />
+      <AppWrapper {...props} lang="de">
+        <CommercialPlant />
+      </AppWrapper>
+    </>
+  );
+};
+
+export default Plant;
