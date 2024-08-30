dbf43c8246f2a6dca9aae2fe2a605abeb1e21960
Alex Barakhov <alex.barakhov@tui.com>
2024-08-30 20:48:42 +0200

    wip: plan page img grid1 component

--- src/ui/pages/CommercialPlant.jsx
+++ src/ui/pages/CommercialPlant.jsx
@@ -7,6 +7,7 @@ import Model3D from "../components/Model3D";
 import Press from "../components/Press";
 import Contact from "../components/Contact";
 import Quote from "../components/Quote";
+import ImageGrid1 from "../components/commercial-plant/ImageGrid1";
 import useTranslations from "../../hooks/useTranslations";
 import { HOME_HERO_VIDEO, HOME_MODEL_3D } from "../../constants/mediaUrls";
 import thomas from "../../assets/images/thomas.png";
@@ -25,33 +26,7 @@ const CommercialPlant = () => {
       <Model3D src={HOME_MODEL_3D}>
         <div className="wrapper custom-cursor_text view">diff content here</div>
       </Model3D>
-      <section className="commercial-plant-grid-1">
-        <div className="wrapper">
-          <div className="image-grid">
-            <div className="image-grid-col-1">
-              <div className="image-grid-item">img1</div>
-              <div className="image-grid-item">img2</div>
-            </div>
-            <div className="image-grid-col-2">
-              <div className="image-grid-item">img3</div>
-              <div className="image-grid-item image-grid-item--content">
-                <h4>World's First Plant Demonstrating Commercial Viability</h4>
-                <p>
-                  TURN2X's plant in Miajadas, Spain, marks a global milestone in
-                  commercial viability by selling 100% of its renewable natural
-                  gas output via longterm off-take contracts. This achievement
-                  underscores TURN2X's pioneering role in sustainable ener gy
-                  solutions.
-                </p>
-              </div>
-
-              <div className="image-grid-item image-grid-item--map">
-                map img
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
+      <ImageGrid1/>
       <section className="commercial-plant-quote">
         <div className="wrapper">
           <Quote
