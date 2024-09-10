0c2dc54921e32627ab0eeef26f6981fc9b92eea8
Alex Barakhov <alex.barakhov@tui.com>
2024-09-10 13:25:34 +0200

    fixes from merged branch

--- src/ui/components/commercial-plant/ImageGrid1.jsx
+++ src/ui/components/commercial-plant/ImageGrid1.jsx
@@ -25,15 +25,15 @@ const ImageGrid1 = () => {
               <img src={imageGrid13} alt="plant" loading="lazy" />
             </div>
             <div className="image-grid-item image-grid-item--content">
-              <h4 className="alpha">
-                {t("plant-grid-1:title")}
-              </h4>
-              <p className="text-xl">
-                {t("plant-grid-1:text")}
-              </p>
+              <h4 className="alpha">{t("plant-grid-1:title")}</h4>
+              <p className="text-xl">{t("plant-grid-1:text")}</p>
             </div>
             <div className="image-grid-item image-grid-item--map">
-              <a href="/" target="_blank" rel="noopener noreferrer">
+              <a
+                href="https://www.google.com/maps/place/Miajadas,+C%C3%A1ceres/@39.1557412,-5.9133892,7225m/data=!3m1!1e3!4m6!3m5!1s0xd144d5d1672a3db:0xa4384d535d448d92!8m2!3d39.1547405!4d-5.9021324!16s%2Fm%2F02z7k44?entry=ttu&g_ep=EgoyMDI0MDkwNC4wIKXMDSoASAFQAw%3D%3D"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
                 <img src={imageGrid1map} alt="plant" loading="lazy" />
               </a>
             </div>
