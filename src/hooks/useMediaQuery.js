3b61bd067a87f471ec245262cdef55c32020b9b3
Alex Barakhov <alex.barakhov@tui.com>
2024-09-19 09:33:14 +0200

    fix: window undefined on build

--- src/hooks/useMediaQuery.js
+++ src/hooks/useMediaQuery.js
@@ -1,9 +1,18 @@
 import { useState, useEffect } from "react";
 
 const useMediaQuery = (query) => {
-  const [matches, setMatches] = useState(window.matchMedia(query).matches);
+  const [matches, setMatches] = useState(() => {
+    if (typeof window !== "undefined") {
+      return window.matchMedia(query).matches;
+    }
+    return false; // or your default value for non-browser environments
+  });
 
   useEffect(() => {
+    if (typeof window === "undefined") {
+      return; // Exit early if we're not in the browser
+    }
+
     const mediaQuery = window.matchMedia(query);
     const handleChange = (e) => setMatches(e.matches);
 
